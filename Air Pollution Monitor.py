0a0f30dcd39162a4c47f817d2f42a2c87487ddb6
allenx555 <allenx555@163.com>
2017-10-24 22:40:58 +0800

    Main, First commit

---
+++ Air Pollution Monitor.py
@@ -0,0 +1,16 @@
+import requests
+from pushbullet import Pushbullet
+from datetime import datetime
+
+
+url = "http://www.pm25.in/api/querys/pm2_5.json?city=chengdu&token=5j1znBVAsnSf5xQyNQyq&stations=no"
+r = requests.get(url)
+response_dict = r.json()
+a = str(response_dict).replace("[{"," ").replace("}]","").replace(",","\n").replace("'","")
+
+pb = Pushbullet("o.6OvO8m6uoRoxjkD4v8ll9TuHrNWTg9RA")
+
+dt = datetime.now()
+t = dt.strftime("%Y-%m-%d %H:%M")
+
+push = pb.push_note(t+" 成都的空气污染", a)

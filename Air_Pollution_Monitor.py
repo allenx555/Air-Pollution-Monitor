3dbf9c6e62f36c38b607cee67c4599565953c143
allenx555 <allenx555@163.com>
2017-10-31 12:13:08 +0800

    add aqi judgment

--- Air_Pollution_Monitor.py
+++ Air_Pollution_Monitor.py
@@ -7,7 +7,14 @@ from datetime import datetime
 url = "http://www.pm25.in/api/querys/pm2_5.json?city=chengdu&token=5j1znBVAsnSf5xQyNQyq&stations=no"
 r = requests.get(url)
 response_dict = r.json()
-a = str(response_dict).replace("[{"," ").replace("}]","").replace(",","\n").replace("'","")
+
+for i in response_dict:
+    m = i.get('aqi')
+    if m <= 50:
+        n = "\n空气质量不错，开窗通风吧"
+    elif m >= 150:
+        n = "\n空气质量很糟糕， 快关窗户为自己续一秒"
+a = str(response_dict).replace("[{"," ").replace("}]","").replace(",","\n").replace("'","") + n
 
 pb = Pushbullet("o.6OvO8m6uoRoxjkD4v8ll9TuHrNWTg9RA")
 
